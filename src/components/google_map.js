860a8abc4e58463a0a5f4d18ac91a1f9dbb91da7
Gintaras Arstikaitis <garstikaitis@gmail.com>
2016-07-03 17:52:20 +0200

    google maps reffered to docs

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -4,6 +4,7 @@ import { GoogleMapLoader, GoogleMap } from 'react-google-maps';
 export default (props) => {
   return (
     <GoogleMapLoader
+      query={{ libraries: "geometry,drawing,places,visualization" }}
       containerElement={ <div style={{height: '100%'}} /> }
       googleMapElement={
         <GoogleMap defaultZoom={12} defaultCenter={{lat: props.lat, lng: props.lon}} />
